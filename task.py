d9fdfa34f5a8c051dc72b6983b5c008e503a4262
Clover <144791981+Clover-LCWC@users.noreply.github.com>
2024-12-10 14:13:41 +0000

    task 1 & 2 done

--- task.py
+++ task.py
@@ -1,9 +1,23 @@
 def task_1():
+    num_resistors = int(input("pls put number: "))
+    total_resistance = 0 
+    
+    for i in range(num_resistors):
+        resistance = float(input(f"enter rsistance {i + 1}: "))
+        total_resistance += resistance
 
+    print(f"Total resistance: {total_resistance}")
     return total_resistance
 
 def task_2(cost):
-
+    vat = cost * 0.2
+    
+    tip_percentage = -1
+    while tip_percentage < 0:
+	    tip_percentage = float(input("enter tip percentage (BIGGER THAN 0 PRETTY PLEASE ^_^): "))
+    
+    tip = (tip_percentage / 100) * cost
+    total_cost = cost + tip + vat
 
     return total_cost
 
@@ -20,4 +34,4 @@ def task_4():
 def task_5():
     height = None
 
-    return height
+    return 
